a1523c500cb0952205a7f0f663087c58ba236160
18796919752 <1356725006@qq.com>
2024-06-13 18:34:36 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ Study/Arrays/Array_Test1.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+var array1 = [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+var array2 = [100]int{1, 3, 99}
+var array3 = [10]string{}
+
+func main() {
+
+	fmt.Println(len(array1))
+	fmt.Println(array1[9])
+	for i, v := range array1 {
+		fmt.Println(i, v)
+	}
+	fmt.Println(array2[len(array2)-1], len(array2))
+	for _, v := range array2 {
+		fmt.Println(v)
+	}
+
+}
--- Study/Basic_Grammar/Test2.go
+++ Study/Basic_Grammar/Test2.go
@@ -1,14 +1,14 @@
 package main
 
-import "fmt"
-
-//var y := 100
-//y := 100
-
-func main() {
-	var i, j int = 1, 2
-	k := 3
-	c, python, java := true, false, "no!"
-
-	fmt.Println(i, j, k, c, python, java, y)
-}
+//import "fmt"
+//
+////var y := 100
+////y := 100
+//
+//func main() {
+//	var i, j int = 1, 2
+//	k := 3
+//	c, python, java := true, false, "no!"
+//
+//	fmt.Println(i, j, k, c, python, java, y)
+//}
