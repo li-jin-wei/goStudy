cbb2a7b9af1fae1180c2e4289514d8d9f3bf42ef
小幸福 <1356725006@qq.com>
2024-06-14 22:47:41 +0800

    commit map test Demo

--- Study/maps/map_Test1.go
+++ Study/maps/map_Test1.go
@@ -1,6 +1,19 @@
 package main
 
-func main() {
-	maps1 = make(map[int]string)
+import "fmt"
 
+func main() {
+	maps1 := map[int]string{1: "a", 2: "b", 3: "c", 4: "d"}
+	fmt.Println(maps1)
+
+	maps2 := make(map[int]int)
+	maps2[1] = 1
+	maps2[100] = 100
+	fmt.Println(maps2)
+	for nums := range maps1 {
+		fmt.Println(maps1[nums])
+	}
+	for key, value := range maps1 {
+		fmt.Println(key, value)
+	}
 }
