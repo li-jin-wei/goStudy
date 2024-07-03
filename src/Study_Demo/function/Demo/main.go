68ecbee2c6b77446d128422626699f4dc05d6a5d
小幸福 <1356725006@qq.com>
2024-07-03 18:31:42 +0800

    Merge remote-tracking branch 'origin/master'

--- src/Study_Demo/function/Demo/main.go
+++ src/Study_Demo/function/Demo/main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"strconv"
+	"fmt"
+	"go-project/src/Study_Demo/function"
 )
 
-func TestDemo(s1 string, i2 int) (s2 string, i1 int) {
-	s2 = strconv.Itoa(i2)
-	i1, _ = strconv.Atoi(s1)
-	println(i1, s2)
-	return s1, i1
-}
-
 func main() {
-	TestDemo("12345667788", 123)
-	TestDemo("nihonium", 1234567)
+	//function.TestDemo("12345667788", 123)
+	//function.TestDemo("nihonium", 1234567)
+	fmt.Println(function.Swap(11, 21))
+
 }
---
+++ src/Study_Demo/function/funcTest.go
@@ -0,0 +1,10 @@
+package function
+
+import "strconv"
+
+func TestDemo(s1 string, i2 int) (s2 string, i1 int) {
+	s2 = strconv.Itoa(i2)
+	i1, _ = strconv.Atoi(s1)
+	println(i1, s2)
+	return s1, i1
+}
