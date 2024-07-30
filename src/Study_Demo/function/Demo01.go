817a3afb5088c5e900446a9d35ce00f957aa74da
小幸福 <1356725006@qq.com>
2024-07-30 15:22:24 +0800

    Merge remote-tracking branch 'origin/master'

--- src/Study_Demo/function/Demo01.go
+++ src/Study_Demo/function/Demo01.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
-	test
+	fmt.Println(max(99999999, 10000000))
+}
+
+func max(num1 int, num2 int) int {
+	var result int
+	if num2 > num1 {
+		result = num2
+	} else {
+		result = num1
+	}
+	return result
 }
