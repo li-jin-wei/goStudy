96fda496b217f1f7720c123b0a078a5c60fff84f
小幸福 <1356725006@qq.com>
2024-07-30 15:12:41 +0800

    Merge remote-tracking branch 'origin/master'

--- src/Study_Demo/function/Demo01.go
+++ src/Study_Demo/function/Demo01.go
@@ -1,19 +1,5 @@
 package main
 
-import "fmt"
-
-// copy
 func main() {
-	//是将funcA赋值给了h,
-	//并不是接受了funcA的返回值，所以结果不为nil
-	h := funcA
-	if h == nil {
-		fmt.Println("h is nil")
-	} else {
-		fmt.Println("h is not nil")
-	}
-}
 
-func funcA() []string {
-	return nil
 }
--- src/Study_Demo/function/SwapDemo.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func Swap(a, b int) (int, int) {
-	return b, a
-}
